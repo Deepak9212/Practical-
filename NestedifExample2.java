a390e36ea8596f99e24e2f5f4152a2ae139fdca0
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-01-24 17:30:05 +0530

    Add files via upload

---
+++ NestedifExample2.java
@@ -0,0 +1,24 @@
+class NestedifExample2{
+public static void main(String[]args){
+
+int num1=2;
+int num2=3;
+
+
+if (num1==3){
+if (num2==5){
+
+System.out.println("wrong statement");
+}
+
+else{ 
+System.out.println("not matched");
+}
+}
+else{
+System.out.println("1st condition not satisfy ");
+}
+}
+}
+
+
