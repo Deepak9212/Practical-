12ecbef99312a3552887f07da34a43aa037969df
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-02-15 23:07:37 +0530

    Inheritance

--- Student.java
+++ Student.java
@@ -1,25 +1,27 @@
-class Student1{      //defining a student class
-int slno;            //defining field/data mmember/instance variable
-String name;
-} 
-class Student2{
-int slno;
-String name;
-}
-class Student{
+package Inheritance;
 
-public static void main(String[]args){  //method of student class
-Student1 s=new Student1();   //creating an object
-Student2 t=new Student2();
-s.slno=01;
-s.name="Deepak";
-System.out.println(s.slno);
-System.out.println(s.name);
+public class Student { //class
+	int id;
+	String name;
+	Address address; //Aggretion (entity ref of address class)
+	public Student(int id,Address address,String name) {
+		this.id=id;
+		this.name=name;
+		this.address=address;
+		
+	}
+	void display() {
+		System.out.println(id+" "+name);
+		System.out.println(address.city+" "+address.state+" "+address.country);
+		
+	}
+	public static void main(String[] args) {
+		Address address1=new Address("Madhubani","Bihar","India");
+		Address address2=new Address("Darbhanga"+ "","Bihar","India");
+		Student s=new Student(4,address1,"Deepak");
+		Student s1=new Student(4,address2,"Jaipur");
+		s.display();
+		s1.display();
+	}
 
-s.slno=02;
-s.name="Sonu";
-System.out.println(s.slno);
-System.out.println(s.name);
-
-}
 }
